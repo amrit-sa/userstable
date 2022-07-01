ff3a901d802a10763bdd973c7e1010933d679f8c
Amrit Singh <techmanmotion@gmail.com>
2022-07-02 02:54:39 +0530

    back to square one

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-// import Users from './components/users/Users';
-// import UserDetail from './components/users/UserDetail';
-// import {Routes, Route,Navigate} from 'react-router-dom';
-// import GlobleState from './context/GlobleState';
-import React from 'react';
-
 
 function App() {
   return (
     <div className="App">
-      <h1>Users Table</h1>
-       {/* <GlobleState>
-        <Routes>
-          <Route path="/" element={<Navigate to="/users" />} />
-          <Route index exact path='/users' element={<Users/>}/>
-          <Route path='/users/:id' element={<UserDetail/>}/>
-        </Routes>
-      </GlobleState> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
